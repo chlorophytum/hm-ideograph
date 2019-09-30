2eda12019e94f964bd9c303a9bed62a72ab2e7a4
be5invis <belleve@typeof.net>
2019-09-29 21:34:33 -0700

    Avoid large stroke length diff considered symmetric

--- liblet/ideograph-shape-analyzer-1/src/analyze/post-stem/symmetry.ts
+++ liblet/ideograph-shape-analyzer-1/src/analyze/post-stem/symmetry.ts
@@ -8,12 +8,12 @@ export default function analyzeSymmetry(
 	strategy: HintingStrategy
 ) {
 	let sym: boolean[][] = [];
-	const limit = strategy.UPM / (strategy.SYMMETRY_TEST_PPEM * 2);
 	const limitX = strategy.UPM / strategy.SYMMETRY_TEST_PPEM;
+	const limitY = strategy.UPM / (strategy.SYMMETRY_TEST_PPEM * 2);
 	for (let j = 0; j < stems.length; j++) {
 		sym[j] = [];
 		for (let k = 0; k < j; k++) {
-			const yDiff = Math.abs(stems[j].y - stems[k].y);
+			const yTopDiff = Math.abs(stems[j].y - stems[k].y);
 			const yBotDiff = Math.abs(stems[j].y - stems[j].width - stems[k].y + stems[k].width);
 			const lengthDiff = Math.abs(
 				stems[j].xMaxExP - stems[j].xMinExP - (stems[k].xMaxExP - stems[k].xMinExP)
@@ -22,9 +22,9 @@ export default function analyzeSymmetry(
 			const topologicalSimilar =
 				!directOverlaps[j][k] && !stems[j].diagHigh && !stems[k].diagHigh;
 			const positionalSimilar =
-				yDiff < limit &&
-				yBotDiff < limit &&
-				Math.max(1, yDiff, yBotDiff) * lengthDiff < limit * limitX;
+				yTopDiff < limitY &&
+				yBotDiff < limitY &&
+				Math.max(limitY / 4, yTopDiff, yBotDiff) * lengthDiff < limitY * limitX;
 			const spatialRelationshipSimilar =
 				(stems[j].hasSameRadicalStemAbove === stems[k].hasSameRadicalStemAbove ||
 					stems[j].hasSameRadicalStemBelow === stems[k].hasSameRadicalStemBelow) &&
