7ff93fd163e417a4163cf646c8db194b33b839e6
be5invis <belleve@typeof.net>
2019-10-06 15:38:09 -0700

    Follow upstream interface updates

--- liblet/ideograph-shape-analyzer-1/src/hinting-model/index.ts
+++ liblet/ideograph-shape-analyzer-1/src/hinting-model/index.ts
@@ -14,56 +14,44 @@ import { GlyphHintTask } from "./glyph-hint";
 import { SharedHintTask } from "./shared-hint";
 
 export class IdeographHintingModel1<GID> implements IHintingModel {
-	constructor(
-		private readonly font: IFontSource<GID>,
-		private readonly ptParams: Partial<HintingStrategy>
-	) {}
+	protected params: HintingStrategy;
+	constructor(private readonly font: IFontSource<GID>, ptParams: Partial<HintingStrategy>) {
+		this.params = createHintingStrategy(font.metadata.upm, ptParams);
+	}
 	public readonly type = "Chlorophytum::IdeographHintingModel1";
 	public readonly allowParallel = false;
 
 	public getHintingTask(ee: IHintingModelExecEnv) {
-		return new IdeographHintingTask(this.font, this.ptParams, ee);
+		return new IdeographHintingTask(this.font, this.params, ee);
 	}
-}
-
-interface EntryHintRecord<GID> {
-	entry: IFontEntry<GID>;
-	parameter: HintingStrategy;
 }
 
 export class IdeographHintingTask<GID> implements ITask<void> {
 	constructor(
 		private readonly font: IFontSource<GID>,
-		private readonly ptParams: Partial<HintingStrategy>,
+		private readonly params: HintingStrategy,
 		private readonly ee: IHintingModelExecEnv
 	) {}
 
 	public async execute(arb: IArbitratorProxy) {
 		// Collect entries from the font source
 		const entries = await this.font.getEntries();
-		const ehRecords: EntryHintRecord<GID>[] = [];
-		for (const entry of entries) {
-			const param = createHintingStrategy(entry.metadata.upm, this.ptParams);
-			ehRecords.push({ entry, parameter: param });
-		}
 
 		// Collect effective glyphs and the first EHR associated to it
-		let glyphs: Map<GID, EntryHintRecord<GID>> = new Map();
-		for (const er of ehRecords) {
-			const ga = new EffectiveGlyphAnalysisTask(er.entry, er.parameter);
+		let glyphs: Set<GID> = new Set();
+		for (const entry of entries) {
+			const ga = new EffectiveGlyphAnalysisTask(entry, this.params);
 			const gs = await arb.demand(ga);
-			for (const g of gs) if (!glyphs.has(g)) glyphs.set(g, er);
+			for (const g of gs) glyphs.add(g);
 		}
 
 		// Do per-glyph hinting
-		const perGlyphHinting = Array.from(glyphs).map(([gid, er]) =>
-			arb.demand(new GlyphHintTask(er.entry, er.parameter, this.ee, gid))
+		const perGlyphHinting = Array.from(glyphs).map(gid =>
+			arb.demand(new GlyphHintTask(this.font, this.params, this.ee, gid))
 		);
 		await Promise.all(perGlyphHinting);
 
 		// Do shared hinting
-		for (const er of ehRecords) {
-			await arb.demand(new SharedHintTask(er.parameter, this.ee));
-		}
+		await arb.demand(new SharedHintTask(this.params, this.ee));
 	}
 }
