533f5897cc8100acc8bd5e155cbeec18e77fb81f
be5invis <belleve@typeof.net>
2019-10-06 00:46:13 -0700

    Recognize common GSUB features

--- liblet/ideograph-shape-analyzer-1/src/hinting-model/effective-glyph-analysis.ts
+++ liblet/ideograph-shape-analyzer-1/src/hinting-model/effective-glyph-analysis.ts
@@ -4,6 +4,27 @@ import { HintingStrategy } from "../strategy";
 
 import { isHangulCodePoint, isIdeographCodePoint } from "./unicode-kind";
 
+const acceptableLookupKinds = new Set([`gsub_single`, `gsub_multiple`, `gsub_alternate`]);
+const acceptableScriptTags = new Set([`hani`, `hang`]);
+const acceptableFeatureTags = new Set([
+	`locl`,
+	`smpl`,
+	`trad`,
+	`tnam`,
+	`jp78`,
+	`jp83`,
+	`jp90`,
+	`jp04`,
+	`hojo`,
+	`nlck`
+]);
+for (let cv = 0; cv <= 99; cv++) {
+	let cvTag: string = "" + cv;
+	while (cvTag.length < 2) cvTag = "0" + cvTag;
+	acceptableFeatureTags.add(`cv` + cvTag);
+	acceptableFeatureTags.add(`ss` + cvTag);
+}
+
 export class EffectiveGlyphAnalysisTask<GID> implements ITask<Set<GID>> {
 	constructor(
 		private readonly font: IFontSource<GID>,
@@ -23,13 +44,25 @@ export class EffectiveGlyphAnalysisTask<GID> implements ITask<Set<GID>> {
 		const related = await this.font.getRelatedGlyphs(gid, ch);
 		if (!related) return;
 		for (const { target, relationTag } of related) {
-			const selector = WellKnownGlyphRelation.UnicodeVariant.unApply(relationTag);
-			if (selector) gidSet.add(target);
+			if (this.relationshipAcceptable(relationTag)) gidSet.add(target);
 		}
 	}
 	private unicodeAcceptable(code: number) {
 		if (isIdeographCodePoint(code) && !this.params.ignoreIdeographs) return true;
 		if (isHangulCodePoint(code) && !this.params.ignoreHangul) return true;
 		return false;
+	}
+	private relationshipAcceptable(relationTag: string) {
+		const selector = WellKnownGlyphRelation.UnicodeVariant.unApply(relationTag);
+		if (selector) return true;
+
+		const gsub = WellKnownGlyphRelation.Gsub.unApply(relationTag);
+		if (!gsub) return false;
+		const [script, language, feature, kind] = gsub;
+		return (
+			acceptableScriptTags.has(script) &&
+			acceptableFeatureTags.has(feature.slice(0, 4)) &&
+			acceptableLookupKinds.has(kind)
+		);
 	}
 }
